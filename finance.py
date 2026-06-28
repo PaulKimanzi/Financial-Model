3e750190dff8e32f5a5f922e17324d6c6fb613cb
Paul Kimanzi <79704376+PaulKimanzi@users.noreply.github.com>
2026-06-28 09:58:38 +0300

    Enhance financial dashboard with new data and chart
    Added calculated columns and updated pie chart rendering.

--- finance.py
+++ finance.py
@@ -2,20 +2,21 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-print("my name")
+
 st.markdown(
     "<h1 style='color:blue;'>Financial Analysis Dashboard</h1>",
     unsafe_allow_html=True
 )
-
+#import the sales Data
 df=pd.read_excel("Product-Sales-Region.xlsx")
 st.write(df.head())
-fig = px.pie(
-    df,
-    names="Product",
-    values="Quantity",
-    title="Sales by Category",
-    hole=0.3  # Optional: creates a donut chart
+
+#Add calculated colums into the dataframe
+df["Month"]=df["OrderDate"].month()
+#Create a Pie chart of Sales by Product Category
+fig = px.pie(df, names="Product", values="Quantity", title="Sales by Category",
+    hole=0.3  #to create a donut chart
 )
 
+#render the charts into the Streamlit Layout
 st.plotly_chart(fig, use_container_width=True)
